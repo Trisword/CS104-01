ea98b323176ff2ab36c17b99e58806bd83b7fe5c
Trisword <olalekan2nd@gmail.com>
2022-10-10 19:32:56 -0400

    Add files via upload

---
+++ conditionsCorrect.py
@@ -0,0 +1,16 @@
+temp = int(input('Please enter the current temperature: '))
+
+if(temp > 90):
+    print("Wear Shorts")
+
+elif(temp > 70):
+    print("Short sleeves are fine")
+
+elif(temp > 50):
+    print("Wear a jacket")
+
+elif(temp > 32):
+    print("Wear a heavy coat")
+
+else:
+    print("Stay Inside")    
